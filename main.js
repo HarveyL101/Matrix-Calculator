d933c789b217c74dbca08a4093942843d52b2dfa
Harvey Lopez <up2118183@myport.ac.uk>
2024-05-07 13:14:18 +0100

    adding formulae for matrices
    added to refer back to once the main functions of add, subtract and multiply are added.

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ class MatrixCalculator {
         this.Bcolumns = 3;
     }
 }
+
+/*
+Formulae for working with matrices:
+
+Addition:
+[1 2]   +   [5 6]   =   [ 6 8 ]         
+[3 4]       [7 8]       [10 12]
+
+Subtraction:
+[5 6]   -   [1 2]   =   [4 4]
+[7 8]       [3 4]       [4 4]
+
+Multiplication:
+# (row[i] * column[i]) + (row[i+1] * column[i+1])
+[a b]   *   [e f]   =   [ae + bg | af + bh]
+[c d]       [g h]       [ce + dg | cf + dh]
+
+*/
