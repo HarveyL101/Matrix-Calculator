f8ef092b8c9c690f93a094611929d33b3c347bde
Harvey Lopez <up2118183@myport.ac.uk>
2024-05-07 11:37:05 +0100

    Beginnings of object
    began building the matrix calculator object itself, commenting out html sliders that can be added back in once core functionality is added.

---
+++ main.js
@@ -0,0 +1,10 @@
+class MatrixCalculator {
+    constructor() {
+        this.matrixA = [];
+        this.matrixB = [];
+        this.Arows = 3;
+        this.Acolumns = 3;
+        this.Brows = 3;
+        this.Bcolumns = 3;
+    }
+}
